2d105f5150fa4795f2b551264b5adbe476697397
Nick Hill <nhill@sdl.com>
2025-08-16 08:22:14 +0200

    Using Axios interceptors for request and response logging

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,8 @@
-import axios from "axios";
+import axios, { InternalAxiosRequestConfig } from "axios";
 
 // CMS API base and UserSessionID cookie
 const CMS_BASE_API_URL = "http://10.100.92.199:81/ui/api/v3.0";
-const USER_SESSION_ID = "CfDJ8MFdR0UUsZtPi5oTnQ5q67LCnIHTVL40uSySitkCCkqYkD7xtDCAVVjMDKEFtGdUtN3zw0deUrkniyzjR334A7dWDEoCavVN6T8pjjR11UjT1Fuk5zl8CVMZqhmG7sZKCkO85GrTkJIDGQSLL2fRF52tdDgtjCxCFJUtkcscP_NuKu1xoNcHAci_RmVs6ZrSCNfknMerZOnBiai_uvUlPYvycbV7Eql4iApNFGWt2pegP-ArPXA6N2kIER_7NadE50EH78qY8vKMper1VwSLe923HJyiW8sYf2u1wZaEOKcmL3E6h0dcHwkvpwY4o5mcvidbd2WcqMrmfToEFlWZGQM";
+const USER_SESSION_ID = "CfDJ8MFdR0UUsZtPi5oTnQ5q67JM3i8Qk6gMmSNuTrfAB59ltGh31Z575PVmIZ_xU3LVGW3xEUKMTmHA67cEhVmec91wxGrLCLC0pj-dvZXwWLFNrr0sxE_ssfvJo4Hq-mhhZXini7eXzv2SQ7KAYRFP6HYUE-RcDVmSDtRVR2vFBFAjH78PM5_oh0AWdbA0OJ6CRrVepnu8QH0CezkeDepTqTfMCTikOSp_GKvU2zsT2mKRFag4SbhLSzsj016DO_EumHCC2godDm0cooqKIF4FUKIr2fww9BacIUkAkvZesIHOLKUwFwfekGM0bv_vnAlZDHfny_hKxhonrv_4-rfpvZE";
 
 export const authenticatedAxios = axios.create({
   baseURL: CMS_BASE_API_URL,
@@ -11,3 +11,49 @@ export const authenticatedAxios = axios.create({
     "Accept": "application/json"
   }
 });
+
+const LOGGING_ENABLED = true;
+
+if (LOGGING_ENABLED) {
+
+  authenticatedAxios.interceptors.request.use(
+    (config: InternalAxiosRequestConfig) => {
+      // This function will run before every request is sent
+
+      console.groupCollapsed(`[AXIOS] ${config.method?.toUpperCase()} Request to ${config.url}`);
+
+      console.log("Full URL:", `${config.baseURL}${config.url}`);
+
+      if (config.params) {
+        console.log("Query Params:", config.params);
+      }
+
+      if (config.data) {
+        console.log("Request Body:", config.data);
+      }
+
+      console.groupEnd();
+
+      // It's crucial to return the config object, otherwise the request will be blocked
+      return config;
+    },
+    (error) => {
+      // This function will run if there's an error setting up the request
+      console.error("[AXIOS] Request Error:", error);
+      return Promise.reject(error);
+    }
+  );
+
+  authenticatedAxios.interceptors.response.use(
+    (response) => {
+      // Any status code that lie within the range of 2xx cause this function to trigger
+      console.log(`[AXIOS] Response from ${response.config.url}:`, response.status, response.data);
+      return response;
+    },
+    (error) => {
+      // Any status codes that falls outside the range of 2xx cause this function to trigger
+      console.error(`[AXIOS] Response Error from ${error.config.url}:`, error.response?.status, error.response?.data);
+      return Promise.reject(error);
+    }
+  );
+}
