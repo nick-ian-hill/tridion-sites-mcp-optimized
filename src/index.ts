abcf4523bcc999f96907d88e78671f37c145cb42
Nick Hill <nhill@sdl.com>
2025-08-16 08:15:33 +0200

    Updated index.ts with new tools.

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,14 @@ import { getPublications } from "./tools/getPublications.js";
 import { search } from "./tools/search.js";
 import { updateComponentById } from "./tools/updateComponentById.js";
 import { updateItemById } from "./tools/updateItemById.js";
+import { localizeItemById } from "./tools/localizeItemById.js";
+import { unlocalizeItemById } from "./tools/unlocalizeItemById.js";
+import { batchLocalizeItemsById } from "./tools/batchLocalizeItemsById.js";
+import { batchUnlocalizeItemsById } from "./tools/batchUnlocalizeItemsById.js";
+import { getCategories } from "./tools/getCategories.js";
+import { getKeywordsForCategory } from "./tools/getKeywordsForCategory.js";
+import { getItemsInContainer } from "./tools/getItemsInContainer.js";
+import { createSchema } from "./tools/createSchema.js";
 
 const server = new McpServer({
   name: "tridion-sites-mcp-server",
@@ -27,6 +35,14 @@ const tools: any[] = [
   updateItemById,
   dependencyGraphForItem,
   getPublications,
+  localizeItemById,
+  unlocalizeItemById,
+  batchLocalizeItemsById,
+  batchUnlocalizeItemsById,
+  getCategories,
+  getKeywordsForCategory,
+  getItemsInContainer,
+  createSchema,
 ];
 
 tools.forEach(tool => {
