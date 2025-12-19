b767c49e1570473afa056159dad0475a0065e9ce
Nick Hill <nhill@sdl.com>
2025-12-19 08:59:22 +0100

    Documented response format

--- src/tools/getSchemaLinks.ts
+++ src/tools/getSchemaLinks.ts
@@ -18,7 +18,7 @@ const schemaPurposeEnum = z.enum([
 
 export const getSchemaLinks = {
     name: "getSchemaLinks",
-    description: "Gets a list of all Schema links within a Publication, filtered by purpose. This tool is useful for finding the available schemas when creating an item or changing an item's metadata schema. For example, use it to find a 'Component' schema before calling 'createComponent', a 'Region' schema before calling 'createRegionSchema', or a 'Metadata' schema before calling 'updateItemProperties'.",
+    description: "Gets a list of all Schema links within a Publication, filtered by purpose. Note that this tool returns a list of Link objects (containing 'IdRef' and 'Title'), unlike other tools that typically return an 'Id'. This is useful for finding the available schemas when creating an item or changing an item's metadata schema. For example, use it to find a 'Component' schema before calling 'createComponent', a 'Region' schema before calling 'createRegionSchema', or a 'Metadata' schema before calling 'updateItemProperties'.",
     input: {
         publicationId: z.string().regex(/^tcm:0-\d+-1$/).describe("The TCM URI of the Publication to search within (e.g., 'tcm:0-5-1'). Use 'getPublications' to find a Publication ID."),
         schemaPurpose: z.array(schemaPurposeEnum).nonempty().describe("An array of one or more Schema purposes to filter the results.")
